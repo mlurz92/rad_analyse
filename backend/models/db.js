43cad21f0daeb0e2b3117995de9f8444a0658c33
mlurz92 <72194266+mlurz92@users.noreply.github.com>
2024-10-23 16:58:23 +0200

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,56 +1,44 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const fs = require('fs');
 const dotenv = require('dotenv');
 
-// Laden der Umgebungsvariablen
 dotenv.config();
 
 const dbPath = path.resolve(__dirname, '..', process.env.DATABASE_URL);
-
-// Sicherstellen, dass das Datenbankverzeichnis existiert
-const dbDir = path.dirname(dbPath);
-if (!fs.existsSync(dbDir)) {
-    fs.mkdirSync(dbDir, { recursive: true });
-}
-
-// Verbindung zur SQLite-Datenbank herstellen
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
-        console.error('Fehler beim Verbinden zur SQLite-Datenbank:', err.message);
+        console.error('Fehler beim Verbinden zur Datenbank:', err.message);
     } else {
-        console.log('Verbindung zur SQLite-Datenbank hergestellt.');
-        initializeDatabase();
+        console.log('Verbunden zur SQLite-Datenbank.');
     }
 });
 
-// Funktion zur Initialisierung der Datenbank
-function initializeDatabase() {
-    const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS investigations (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            Modalitaet TEXT NOT NULL,
-            Studiendatum TEXT NOT NULL,
-            Studienbeschreibung TEXT,
-            AnfragendeAbteilung TEXT,
-            AnfragenderArzt TEXT,
-            BefundVerfasser TEXT,
-            Diagnose TEXT,
-            Untersuchungsstatus TEXT,
-            Institution TEXT,
-            Anfragename TEXT,
-            Patientengeschlecht TEXT,
-            Patientenalter TEXT,
-            Überweiser TEXT
-        );
-    `;
-    db.run(createTableQuery, (err) => {
-        if (err) {
-            console.error('Fehler beim Erstellen der Tabelle:', err.message);
-        } else {
-            console.log('Tabelle "investigations" ist bereit.');
-        }
-    });
-}
+// Erstellung der investigations-Tabelle, falls sie noch nicht existiert
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS investigations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Modalitaet TEXT,
+        Studiendatum TEXT,
+        Studienbeschreibung TEXT,
+        AnfragendeAbteilung TEXT,
+        AnfragenderArzt TEXT,
+        BefundVerfasser TEXT,
+        Diagnose TEXT,
+        Untersuchungsstatus TEXT,
+        Institution TEXT,
+        Anfragename TEXT,
+        Patientengeschlecht TEXT,
+        Patientenalter TEXT,
+        Überweiser TEXT
+    )
+`;
+
+db.run(createTableQuery, (err) => {
+    if (err) {
+        console.error('Fehler beim Erstellen der Tabelle:', err.message);
+    } else {
+        console.log('Tabelle investigations ist bereit.');
+    }
+});
 
 module.exports = db;
