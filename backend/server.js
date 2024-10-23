91095db5e3196b2249292ff306acb74f5965cfb4
mlurz92 <72194266+mlurz92@users.noreply.github.com>
2024-10-23 16:59:12 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,9 @@ const dotenv = require('dotenv');
 const uploadRoutes = require('./routes/uploadRoutes');
 const filterRoutes = require('./routes/filterRoutes');
 
-// Laden der Umgebungsvariablen
 dotenv.config();
 
-// Erstellung der Express-Anwendung
 const app = express();
-const PORT = process.env.PORT || 3001; // Standardport 3001
-const HOST = process.env.HOST || '0.0.0.0';
 
 // Sicherheitsmiddleware
 app.use(helmet());
@@ -27,19 +23,22 @@ app.use(cors({
 // Middleware zum Parsen von JSON-Daten
 app.use(express.json());
 
-// Nutzung von Upload- und Filter-Routen
-app.use('/', uploadRoutes);
-app.use('/', filterRoutes);
+// Nutzung von Upload- und Filter-Routen unter dem Pfad /rad_analyse
+app.use('/rad_analyse', uploadRoutes);
+app.use('/rad_analyse', filterRoutes);
 
 // Servieren der statischen Frontend-Dateien
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/rad_analyse', express.static(path.join(__dirname, '../public')));
 
 // Catch-All Route für Frontend-Routing (Single Page Application)
-app.get('*', (req, res) => {
+app.get('/rad_analyse/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
 // Starten des Servers
+const PORT = process.env.PORT || 3001;
+const HOST = process.env.HOST || '0.0.0.0';
+
 app.listen(PORT, HOST, () => {
     console.log(`Backend läuft auf Port ${PORT}`);
 });
